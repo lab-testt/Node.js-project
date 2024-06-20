8386735c13907e3b5e24be6220323f03fdeb8811
lab-testt <saydhany012@gmail.com>
2024-06-20 20:31:15 +0300

    update all

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const express = require('express');
 const { Pool } = require('pg');
+
 const app = express();
 const port = 3000;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'postgres',
-  database: 'mydb',
-  password: 'password',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL
 });
 
 app.get('/', async (req, res) => {
+  const client = await pool.connect();
   try {
-    const client = await pool.connect();
     const result = await client.query('SELECT NOW()');
-    res.send(result.rows[0]);
+    res.send(`Database time: ${result.rows[0].now}`);
+  } finally {
     client.release();
-  } catch (err) {
-    console.error(err);
-    res.send('Error ' + err);
   }
 });
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
+  console.log(`Server running on port ${port}`);
 });
